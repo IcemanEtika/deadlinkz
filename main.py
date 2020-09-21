e92efe2b0a9dd67bc2ebbcbd55339a4bd8c2dc8d
TJ LeBlanc <ssjarceus@outlook.com>
2020-09-21 13:16:53 -0400

    Created first version of project

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+import sys
+import re
+
+from colorama import Fore, init
+init()
+
+
+def checkURL():
+    with open(sys.argv[1], "r") as f:
+        url = f.read()
+        links = re.findall("https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+", url)
+
+    for link in links:
+        print(link)
+        r = requests.get(link)
+        if str(r) == "<Response [200]>":
+            print(Fore.GREEN + str(r) + " Response Good!" + Fore.RESET)
+        elif str(r) == "<Response [400]>" or str(r) == "<Response [404]>":
+            print(Fore.RED + str(r) + " Response Bad! There may be a problem with the website." + Fore.RESET)
+        else:
+            print(str(r) + " Response Unknown!")
+
+
+if len(sys.argv) > 1:
+    if sys.argv[1] != "v" and sys.argv[1] != "version":
+        checkURL()
+    else:
+        print("Dead Linkz v0.1")
+        print("Made by IcemanEtika")
+else:
+    print('\033[91m' + "No arguments inputted. The correct usage is main.py insert_website_here.html" + '\033[0m')
