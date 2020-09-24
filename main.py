cf12e973f3984e9452d9f1ca50b48e98b939986c
TJ LeBlanc <ssjarceus@outlook.com>
2020-09-24 15:28:17 -0400

    Implemented argparse and put links and response messages on the same line

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import requests
 import sys
 import re
@@ -7,26 +8,28 @@ init()
 
 
 def checkURL():
-    with open(sys.argv[1], "r") as f:
-        url = f.read()
-        links = re.findall(r'https?://[^\s<>"].[^\s<>"]+', url)
+    try:
+        with open(sys.argv[2], "r") as f:
+            url = f.read()
+            links = re.findall(r'https?://[^\s<>"].[^\s<>"]+', url)  # find all urls and add them to the links array
 
-    for link in links:
-        print(link)
-        r = requests.get(link)
-        if 200 <= r.status_code <= 299:
-            print(Fore.GREEN + str(r) + " Response Good!" + Fore.RESET)
-        elif 400 <= r.status_code <= 499:
-            print(Fore.RED + str(r) + " Response Bad! There may be a problem with the webpage." + Fore.RESET)
-        else:
-            print(str(r) + " Response Unknown!")
+        for link in links:
+            r = requests.get(link)  # gets the status code of the website
+            if 200 <= r.status_code <= 299:
+                print(Fore.GREEN + str(link) + " " + str(r) + " Good!")
+            elif 400 <= r.status_code <= 599:
+                print(Fore.RED + str(link) + " " + str(r) + " Bad! There may be a problem with the webpage.")
+            else:
+                print(str(link) + " " + str(r) + " Unknown!")
+    except FileNotFoundError:
+        print(Fore.RED + "Error: the file could not be opened.");
 
 
-if len(sys.argv) > 1:
-    if sys.argv[1] != "v" and sys.argv[1] != "version":
-        checkURL()
-    else:
-        print("deadlinkz v0.1")
-        print("Made by IcemanEtika")
-else:
-    print('\033[91m' + "No arguments inputted. The correct usage is main.py insert_website_here.html\nFor version info type main.py followed by either v or version" + '\033[0m')
+# Create parser that allows for arguments to be used with the tool (-c, --check, -v, --version, -h, --help)
+parser = argparse.ArgumentParser(description='Checks for dead urls in a file')
+parser.add_argument('-c', '--check', help='Checks urls in text file (e.g, main.py -c index.html)')
+parser.add_argument('-v', '--version', action="version", version='deadlinkz v0.1', help='Displays version info')
+args = parser.parse_args()
+
+checkURL()
+
