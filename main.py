e2473cadcb18d9dc492d0c15d3031a78a70d87e6
TJ LeBlanc <ssjarceus@outlook.com>
2020-09-24 21:54:52 -0400

    Added more links to test file

--- main.py
+++ main.py
@@ -14,19 +14,22 @@ def checkURL():
             links = re.findall(r'https?://[^\s<>"].[^\s<>"]+', url)  # find all urls and add them to the links array
 
         for link in links:
-            r = requests.get(link, timeout=15)  # gets the status code of the website
-            if 200 <= r.status_code <= 299:
-                print(Fore.GREEN + str(link) + " " + str(r) + " Good!")
-            elif 400 <= r.status_code <= 599:
-                print(Fore.RED + str(link) + " " + str(r) + " Bad! There may be a problem with the webpage.")
-            else:
-                print(str(link) + " " + str(r) + " Unknown!")
+            try:
+                r = requests.get(link, timeout=15)  # gets the status code of the website
+                if 200 <= r.status_code <= 299:
+                    print(Fore.GREEN + str(link) + " " + str(r) + " Good!")
+                elif 400 <= r.status_code <= 599:
+                    print(Fore.RED + str(link) + " " + str(r) + " Bad! There may be a problem with the webpage.")
+                else:
+                    print(str(link) + " " + str(r) + " Unknown!")
+            except requests.exceptions.RequestException:
+                print(Fore.RED + "Error: could not connect to website!")
+                continue
+            except requests.exceptions.Timeout:
+                print(Fore.RED + "Error: connection to website timed out!")
+                continue
     except FileNotFoundError:
         print(Fore.RED + "Error: the file could not be opened.")
-    except requests.exceptions.RequestException:
-        print(Fore.RED + "Error: could not connect to website!")
-    except requests.exceptions.Timeout:
-        print(Fore.RED + "Error: connection to website timed out!")
 
 
 # Create parser that allows for arguments to be used with the tool (-c, --check, -v, --version, -h, --help)
@@ -36,4 +39,3 @@ parser.add_argument('-v', '--version', action="version", version='deadlinkz v0.1
 args = parser.parse_args()
 
 checkURL()
-
