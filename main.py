7d5f3a6878a51215815718cf835792be5d75c83d
TJ LeBlanc <ssjarceus@outlook.com>
2020-10-21 00:01:45 -0400

    Updated main.py for improved maintainability
      * optimized ignore links code
    
      * change file read order when using -i and --ignore
    
      * update function names for better readability
    
      * add exit code to unignored links function
    
      * updated ignore-urls.txt

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from colorama import Fore, init
 init()
 
 
-def checkGood(links):
+def checkGoodLinks(links):
     for link in links:
         try:
             r = requests.head(link, timeout=10)  # gets the status code of the website
@@ -18,7 +18,7 @@ def checkGood(links):
             continue
 
 
-def checkBad(links):
+def checkBadLinks(links):
     for link in links:
         try:
             r = requests.head(link, timeout=10)  # gets the status code of the website
@@ -32,111 +32,62 @@ def checkBad(links):
         finally:
             continue
 
-def checkWithoutIgnoredLink(links):
+
+def checkUnignoredLinks(links):
     try:
-        # Check the ignored text file from argument[3]
-        with open(sys.argv[3], "r") as i:
-            texts = i.read().splitlines();
+        with open(sys.argv[2], 'r') as f:
+            line = f.read()
+            if line[0] == "#":
+                comment = True
+            ignoredUrls = re.findall(r'https?://[^\s<>"].[^\s<>"]+', line)
+        if len(ignoredUrls) == 0 and comment is False:
+            raise FileNotFoundError;
+        urls = [x for x in links if x not in ignoredUrls]  # remove all ignored urls then check result
+        checkLinks(urls)
+    except FileNotFoundError:
+        print(Fore.RED + "Error: invalid text file.")
+        sys.exit(2)
 
-        if isValidTextFile(texts):
-            # collect URL from text file
-            r = re.compile('https?://[^\s<>"].[^\s<>"]+')
-            ignoredLinks = list(filter(r.match, texts))
-            ignored = False
 
-            for link in links:
-                try:
-                    if len(ignoredLinks) > 0:
-                        for ig in ignoredLinks:
-                            if re.match(ig, link):
-                                ignored = True
-                                break
-                        if ignored:
-                            ignored = False
-                            continue
-                        else:
-                            ignored = False
-                            r = requests.head(link, timeout=10)  # gets the status code of the website
-                            if 200 <= r.status_code <= 299:
-                                print(Fore.GREEN + str(link) + " " + str(r) + " Good!" + Fore.RESET)
-                            elif 400 <= r.status_code <= 599:
-                                print(Fore.RED + str(link) + " " + str(r) + " Bad! There may be a problem with the webpage." + Fore.RESET)
-                                exitCode.append(1);
-                            else:
-                                print(str(link) + " " + str(r) + " Unknown!")
-                        
-                        
-                    else:
-                        r = requests.head(link, timeout=10)  # gets the status code of the website
-                        if 200 <= r.status_code <= 299:
-                            print(Fore.GREEN + str(link) + " " + str(r) + " Good!" + Fore.RESET)
-                        elif 400 <= r.status_code <= 599:
-                            print(Fore.RED + str(link) + " " + str(r) + " Bad! There may be a problem with the webpage." + Fore.RESET)
-                            exitCode.append(1);
-                        else:
-                            print(str(link) + " " + str(r) + " Unknown!")
-                except requests.exceptions.RequestException:
-                    print(Fore.RED + "Error: could not connect to website!")
-                except requests.exceptions.Timeout:
-                    print(Fore.RED + "Error: connection to website timed out!")
-                finally:
-                    exitCode = 1;
-                    continue
-            
-        else:
-            raise NotImplementedError
-    except NotImplementedError:
-        exitCode = 2;
-        print(Fore.RED + "Error: Invalid text file")   
-
-# Check the available text file that include all of condition for lab4 and return boolean 
-def isValidTextFile(links):
-    if len(links) == 0: # Check empty file, If file is empty, Check every URLs
-        result = True
+def checkLinks(links):
     for link in links:
-        if link[0] == "#": # '#' is comment line
-            result = True
-        elif re.match(r'https?://[^\s<>"].[^\s<>"]+', link): # Check URL that include (http://, https://)
-            result = True
-        else: # if not, Go to ignore text file error
-            return False
-    return result
+        try:
+            r = requests.head(link, timeout=10)  # gets the status code of the website
+            if 200 <= r.status_code <= 299:
+                print(Fore.GREEN + str(link) + " " + str(r) + " Good!" + Fore.RESET)
+            elif 400 <= r.status_code <= 599:
+                print(Fore.RED + str(link) + " " + str(r) + " Bad! There may be a problem with the webpage." + Fore.RESET)
+                exitCode.append(1);
+            else:
+                print(str(link) + " " + str(r) + " Unknown!")
+        except requests.exceptions.RequestException:
+            print(Fore.RED + "Error: could not connect to website!")
+        except requests.exceptions.Timeout:
+            print(Fore.RED + "Error: connection to website timed out!")
+        finally:
+            exitCode = 1;
+            continue
 
-def checkURL():
+
+def openFile():
     exitCode = 0;
     try:
-        with open(sys.argv[2], "r") as f:
+        with open(sys.argv[3], "r") as f:
             links = re.findall(r'https?://[^\s<>"].[^\s<>"]+', f.read())  # find all urls and add them to the links array
-
         if sys.argv[1] == "-g" or sys.argv[1] == "--good":
-            checkGood(links)
+            checkGoodLinks(links)
         elif sys.argv[1] == "-b" or sys.argv[1] == "--bad":
-            checkBad(links)
-        elif sys.argv[1] == "-i" or sys.argv[1] == "--ignore": 
-            checkWithoutIgnoredLink(links)
+            checkBadLinks(links)
+        elif sys.argv[1] == "-i" or sys.argv[1] == "--ignore":
+            checkUnignoredLinks(links)
         else:
-            for link in links:
-                try:
-                    r = requests.head(link, timeout=10)  # gets the status code of the website
-                    if 200 <= r.status_code <= 299:
-                        print(Fore.GREEN + str(link) + " " + str(r) + " Good!" + Fore.RESET)
-                    elif 400 <= r.status_code <= 599:
-                        print(Fore.RED + str(link) + " " + str(r) + " Bad! There may be a problem with the webpage." + Fore.RESET)
-                        exitCode.append(1);
-                    else:
-                        print(str(link) + " " + str(r) + " Unknown!")
-                except requests.exceptions.RequestException:
-                    print(Fore.RED + "Error: could not connect to website!")
-                except requests.exceptions.Timeout:
-                    print(Fore.RED + "Error: connection to website timed out!")
-                finally:
-                    exitCode = 1;
-                    continue
+            checkLinks(links)
     except FileNotFoundError:
         exitCode = 2;
         print(Fore.RED + "Error: the file could not be opened.")
 
     sys.exit(exitCode)
+
 
 # Create parser that allows for arguments to be used with the tool (-a, --all, -v, --version, -h, --help)
 parser = argparse.ArgumentParser(description='Checks for dead urls in a file')
@@ -144,8 +95,8 @@ parser.add_argument('-a', '--all', help='Checks urls in text file (e.g, main.py 
 parser.add_argument('-g', '--good', help='Displays all good links in file')
 parser.add_argument('-b', '--bad', help='Displays all bad links in file')
 parser.add_argument('-v', '--version', action="version", version='deadlinkz v0.1', help='Displays version info')
-parser.add_argument('-i', '--ignore', nargs='+', help='Displays all links witout ignored link in file') # Check URLs without ignored file's url
+parser.add_argument('-i', '--ignore', nargs='+', help='Displays all links without ignored link in file') # Check URLs without ignored file's url
 args = parser.parse_args()
 
-threading.Thread(target=checkURL()).start()
+threading.Thread(target=openFile()).start()
 sys.exit(0)
